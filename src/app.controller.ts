589441adb4da606d6da9f8e16a0bee5695222781
Yash Vataliya <yashvataliya65@gmail.com>
2025-10-18 23:36:35 +0530

    assignment-1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/hello')
+  getHelloFromController(): string {
+    return 'Hello from HelloController!';
+  }
 }
